f28a1d03d1ca7edcd75d92234471ad40ed169c1a
PANDI902 <51266266+PANDI902@users.noreply.github.com>
2024-06-21 16:53:49 +0530

    Add files via upload

---
+++ pushd_popd.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+char *dir;
+struct Node *next;
+}Node;
+
+Node *stack = NULL;
+
+void pushd(const char *dir)
+{
+Node *newNode = (Node *)malloc(sizeof(Node));
+if(newNode == NULL)
+{
+perror("malloc error");
+return;
+}
+newNode->dir = strdup(dir);
+if(!newNode->dir)
+{
+perror("strdup error ");
+free(newNode);
+return;
+}
+newNode->next = stack;
+stack = newNode;
+if(chdir(dir)!=0)
+{
+perror("chdir error ");
+free(newNode->dir);
+free(newNode);
+}
+else
+{
+printf("directory pushed: %s\n",dir);
+}
+}
+void popd()
+{
+if(!stack)
+{
+fprintf(stderr,"stack is empty, cannot pop\n");
+return;
+}
+Node *nodeToRemove = stack;
+stack = stack->next;
+
+if(chdir(stack->dir)!=0)
+{
+perror("chdir error");
+}
+else
+{
+printf("Directory popped: %s\n",nodeToRemove->dir);
+}
+free(nodeToRemove->dir);
+free(nodeToRemove);
+}
+
+void printstack()
+{
+Node *current =stack;
+printf("Current Directory stack : \n");
+while(current)
+{
+printf("%s\n",current->dir);
+current = current->next;
+}
+}
+int main()
+{
+char *cwd;
+cwd = getcwd(NULL,0);
+
+if(cwd == 0)
+{
+perror("cwd error");
+return 1;
+}
+printf("Current Working Directory : %s\n",cwd);
+char input[256];
+while(1)
+{
+printf("enter command (pushd <dir>, popd,exit) : ");
+if(fgets(input,sizeof(input),stdin)==NULL)
+{
+perror("directory get :");
+continue;
+}
+input[strcspn(input,"\n")]='\0';
+if(strncmp(input,"pushd",6)==0)
+{
+char *dir = input +6;
+pushd(dir);
+printstack();
+}
+else if(strcmp(input,"popd")==0)
+{
+popd();
+printstack();
+}
+else if(strcmp(input,"exit")==0)
+{
+break;
+}
+else{
+printf("unknown command %s\n",input);
+}
+}
+return 0;
+}
